b611cb4a47dd4c13c9f5c78288014e0e42019b82
josefsanchez <80663226+josefsanchez@users.noreply.github.com>
2021-05-15 18:09:59 -0600

    Delete fase3/adivina directory

--- fase3/adivina/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from adivina.models import Juego
-
-# Register your models here.
-
-admin.site.register(Juego)
--- fase3/adivina/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Juego (models.Model):
-    nombre  = models.CharField(max_length=50)
-    numero  = models.IntegerField()
-    intento = models.IntegerField()
-
